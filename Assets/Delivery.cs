07259677671d41f1e43ed91e898fa8bbe08a26de
Nika Nozadze <nikanozadze1010@gmail.com>
2023-06-11 21:41:36 +0400

    work with package bool

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("oops!");
     }
      private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Package"){
-            Debug.Log("pick up package");
+            hasPackage = true;
         }
-        if(other.tag == "Customer"){
-            Debug.Log("package delivered");
+        if(other.tag == "Customer" && hasPackage){
+            hasPackage = false;
         }
     }
 }
